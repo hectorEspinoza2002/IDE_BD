6a58c13e951beced13d3b2425ac7ff7b14604448
Héctor Espinoza 2002 <hectorfrancisco1214@gmail.com>
2026-03-23 22:43:45 -0600

    Primer commit

---
+++ demoide/src/main/java/com/ide/demoide/DemoideApplication.java
@@ -0,0 +1,17 @@
+package com.ide.demoide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoideApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo desde mi VICTUS");
+		
+		SpringApplication.run(DemoideApplication.class, args);
+
+	}
+
+}
---
+++ demoide/src/test/java/com/ide/demoide/DemoideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ide.demoide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
